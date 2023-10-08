42ea9d17621acb7f0f81ce1416e4fa1651591b41
Sebastian <mrseb10@gmail.com>
2023-10-08 21:51:08 +0200

    Maybe that approach...

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,15 @@
+import styles from '../scss/Calculator.module.scss';
+
 const Calculator = () => {
-	return <div>Calculator</div>;
+	return (
+		<main className={styles.theme}>
+			<div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</main>
+	);
 };
 
 export default Calculator;
